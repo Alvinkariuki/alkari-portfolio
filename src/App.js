3e780d1747c13bb5611d5258b4393ac6e0eed64a
Alvinkariuki <alvinkariuki230@gmail.com>
2021-02-06 10:45:24 +0300

    Carousel Scroll and TitleMsg Components creation

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import "./App.css";
 
 // component imports
 import Navbar from "./components/Navbar/Navbar.component";
+import Carousel from "./components/Carousel/Carousel.component";
+import TitleMsg from "./components/TitleMsg/TitleMsg.component";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Carousel />
+      <TitleMsg />
       My react app
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-import {ThemeProvider} from "styled-components";
-import {theme} from "./themes/theme";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./themes/theme";
 
 import App from "./App";
 
 ReactDOM.render(
-  <ThemeProvider theme={theme}> 
+  <ThemeProvider theme={theme}>
     <React.StrictMode>
       <App />
     </React.StrictMode>
